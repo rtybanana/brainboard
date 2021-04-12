97f01da74bf93a80861c8d201303ebe2539e64a2
rorypinkney <rorypinkney@yahoo.co.uk>
2021-04-12 15:08:39 +0100

    fix for brainaxes loading in github pages changed opacity of glass brain to make ortho plots less busy

--- components/ortho/ortho-x.vue
+++ components/ortho/ortho-x.vue
@@ -1,7 +1,7 @@
 <template>
   <svg ref="axes" id="axes" width="100%" version="1.1" viewBox="0 0 187.2 165.6" xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink">
-    <image width="187.2" height="165.6" href="../../brainaxes/x.svg"/>
+    <image width="187.2" height="165.6" href="./brainaxes/x.svg" opacity="0.65"/>
   </svg>
 </template>
 
